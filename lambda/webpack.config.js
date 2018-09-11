d4bd8fe374c7fe44ef575c6a45a2745c807be3ab
Chandler Fang <chandler.fang@cashrewards.com>
2018-09-11 17:26:15 +1000

    Fix serverless webpack config.

--- lambda/webpack.config.js
+++ lambda/webpack.config.js
@@ -5,39 +5,43 @@ const copyWebpackPlugin = require("copy-webpack-plugin");
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  mode: 'production',
-  entry: slsw.lib.entries,
-  target: 'node',
-  resolve: { extensions: ['.ts', '.js'] },
-  // Make sure we include all node_modules etc
-  externals: [/(node_modules|main\..*\.js)/,],
-  output: {
-    libraryTarget: "commonjs",
-    // Puts the output at the root of the dist folder
-    path: path.join(__dirname, 'dist', 'lambda'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader' }
+    mode: 'production',
+    entry: slsw.lib.entries,
+    target: 'node',
+    resolve: { extensions: ['.ts', '.js'] },
+    optimization: {
+        // keep minimization off
+        // workaround for https://github.com/angular/angular-cli/issues/10635
+        minimize: false
+    },
+    // Make sure we include all node_modules etc
+    externals: [/(node_modules|main\..*\.js)/, ],
+    output: {
+        libraryTarget: "commonjs",
+        // Puts the output at the root of the dist folder
+        path: path.join(__dirname, 'dist', 'lambda'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    },
+    plugins: [
+        new copyWebpackPlugin([
+            { from: "dist/browser/**/*" },
+            { from: "dist/server/**/*" },
+        ]),
+        new webpack.ContextReplacementPlugin(
+            // fixes WARNING Critical dependency: the request of a dependency is an expression
+            /(.+)?angular(\\|\/)core(.+)?/,
+            path.join(__dirname, 'src'), // location of your src
+            {} // a map of your routes
+        ),
+        new webpack.ContextReplacementPlugin(
+            // fixes WARNING Critical dependency: the request of a dependency is an expression
+            /(.+)?express(\\|\/)(.+)?/,
+            path.join(__dirname, 'src'), {}
+        )
     ]
-  },
-  plugins: [
-    new copyWebpackPlugin([
-      {from: "dist/browser/**/*"},
-      {from: "dist/server/**/*"},
-    ]),
-    new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
-      /(.+)?angular(\\|\/)core(.+)?/,
-      path.join(__dirname, 'src'), // location of your src
-      {} // a map of your routes
-    ),
-    new webpack.ContextReplacementPlugin(
-      // fixes WARNING Critical dependency: the request of a dependency is an expression
-      /(.+)?express(\\|\/)(.+)?/,
-      path.join(__dirname, 'src'),
-      {}
-    )
-  ]
 }
